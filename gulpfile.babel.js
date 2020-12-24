82c7e225351f7c5a0363b072b37f37ab99d9cf60
svforg <svforgel89@gmail.com>
2020-12-24 21:49:19 +0500

    del build from repo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,7 +3,12 @@
 const gulp = require('gulp');
 const requireDir = require('require-dir');
 const project = requireDir('./gulp');
+var ghPages = require('gulp-gh-pages');
 
+gulp.task('deploy', function() {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages());
+});
 
 gulp.task('watch',
     project.allFiles.watch
